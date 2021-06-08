fd0f7c2a34bcebe555fe25ab2bda2dec7b9bbf6c
Pat <sparksman@web.de>
2021-06-08 17:09:56 +0200

    Add classes to display stuff

---
+++ src/main/java/Display.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JFrame {
+
+    private DisplayPanel panel;
+
+    public Display(Chip chip) {
+        setPreferredSize(new Dimension(640, 320));
+        pack();
+        setPreferredSize(new Dimension(640 + getInsets().left + getInsets().right, 320 + getInsets().left + getInsets().right));
+        panel = new DisplayPanel(chip);
+        setLayout(new BorderLayout());
+        add(panel, BorderLayout.CENTER);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Chip-8 Interpreter");
+        pack();
+        setVisible(true);
+    }
+}
