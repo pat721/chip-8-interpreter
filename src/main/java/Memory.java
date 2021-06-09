a9fd107eb33d9204bdbe3b6cc16d6466f684eb9b
pat721 <sparksman@web.de>
2021-06-09 21:49:10 +0200

    Add Getter and Setter for Memory
    Added getters and setters for the Memory and changed the datatypeof memory

--- src/main/java/Memory.java
+++ src/main/java/Memory.java
@@ -4,7 +4,7 @@
 public class Memory {
 
     // 4K Bytes of Memory
-    private char[] memory;
+    private byte[] memory;
 
     //16-Level Stack
     private char[] stack;
@@ -13,9 +13,17 @@ public class Memory {
     private byte[] displayBuffer;
 
     public Memory() {
-        memory = new char[4096];
+        memory = new byte[4096];
         stack = new char[16];
         displayBuffer = new byte[64 * 32];
+    }
+
+    public byte getMemory(char address) {
+        return memory[address];
+    }
+
+    public void setMemory(char address, byte content) {
+        memory[address] = content;
     }
 
     // Return Bytebuffer for displaying it
